6106288ff7892dd6be92a524612e11144cc68a7f
yanke <1435020085@qq.com>
2024-05-05 17:06:33 +0800

    esp_demos: add lvgl spiffs

--- examples/ui/lvgl/lvgl_spiffs/main/main.c
+++ examples/ui/lvgl/lvgl_spiffs/main/main.c
@@ -1,27 +1,25 @@
-#include <stdlib.h>
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "esp_check.h"
-#include "nvs_flash.h"
-#include "st7789.h"
+#include <stdio.h>
 #include "esp_lvgl_port.h"
-#include "hal_camera.h"
+#include "st7789.h"
+#include "esp_check.h"
+#include "esp_log.h"
+#include "lvgl.h"
 #include "ui.h"
+#include "hal_spiffs.h"
 
+static const char *TAG = "LVGL_FATFS";
 lv_disp_t *lvgl_disp = NULL;
-lv_obj_t *lvgl_img_cam = NULL;
-static const char *TAG = "LVGL_CAMERA";
+lv_obj_t *lvgl_img = NULL;
+lv_obj_t *lvgl_screen = NULL;
 
 lcd_config_t lcd_config = {
     .spi_host_device = SPI3_HOST,
-    .dc = GPIO_NUM_45,
-    .cs = GPIO_NUM_14,
-    .sclk = GPIO_NUM_21,
-    .mosi = GPIO_NUM_47,
-    .rst = -1,
-    .backlight = GPIO_NUM_48,
+    .dc = GPIO_NUM_4,
+    .cs = GPIO_NUM_5,
+    .sclk = GPIO_NUM_6,
+    .mosi = GPIO_NUM_7,
+    .rst = GPIO_NUM_8,
+    .backlight = GPIO_NUM_9,
     .lcd_bits_per_pixel = 16,
     .lcd_color_space = LCD_COLOR_SPACE_RGB,
     .lcd_height_res = 240,
@@ -29,20 +27,11 @@ lcd_config_t lcd_config = {
     .lcd_draw_buffer_height = 50,
 };
 
-lv_img_dsc_t img_dsc = {
-    .header.always_zero = 0,
-    .header.w = 240,
-    .header.h = 240,
-    .data_size = 240 * 240 * 2,
-    .header.cf = LV_IMG_CF_TRUE_COLOR,
-    .data = NULL,
-};
-
 esp_err_t lvgl_init()
 {
     const lvgl_port_cfg_t lvgl_cfg = {
         .task_priority = 4,      /* LVGL task priority */
-        .task_stack = 5 * 1024,  /* LVGL task stack size */
+        .task_stack = 8 * 1024,  /* LVGL task stack size */
         .task_affinity = 0,      /* LVGL task pinned to core (-1 is no affinity) */
         .task_max_sleep_ms = 10, /* Maximum sleep in LVGL task */
         .timer_period_ms = 5     /* LVGL timer tick period in ms */
@@ -73,37 +62,39 @@ esp_err_t lvgl_init()
     return ESP_OK;
 }
 
-void camera_show_task(void *args)
-{
-    while (1)
-    {
-        camera_fb_t *pic = esp_camera_fb_get();
-        if (pic == NULL)
-        {
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-        else
-        {
-            img_dsc.data = pic->buf;
-            lv_img_set_src(ui_Image1, &img_dsc);
-            esp_camera_fb_return(pic);
-        }
-    }
-}
-
 void app_main(void)
 {
-    // Init lcd st7789
+    // spiffs init
+    ESP_ERROR_CHECK(hal_spiffs_init("/spiffs"));
+
+    // lcd init
     ESP_ERROR_CHECK(lcd_init(lcd_config));
     lcd_fullclean(lcd_panel, lcd_config, rgb565(255, 255, 255));
 
-    // Init camera
-    ESP_ERROR_CHECK(hal_camera_init());
-
-    // Init lvgl
+    // lvgl init
     ESP_ERROR_CHECK(lvgl_init());
     ui_init();
 
-    // Init camera show task
-    xTaskCreatePinnedToCore(&camera_show_task, "camera show", 1024 * 5, NULL, 6, NULL, 1);
+    // add ui
+    lv_obj_t *label = lv_label_create(ui_Screen1);
+    lv_label_set_text(label, "IMG_Decoder");
+    lv_obj_align_to(label, NULL, LV_ALIGN_TOP_MID, 0, 0);
+
+    // add gif
+    lv_obj_t *img_gif = lv_gif_create(ui_Screen1);
+    lv_gif_set_src(img_gif, "S:/spiffs/example.gif");
+    lv_obj_align_to(img_gif, NULL, LV_ALIGN_LEFT_MID, 0, 0);
+
+    // add jpg
+    lv_obj_t *img_jpg = lv_img_create(ui_Screen1);
+    lv_img_set_src(img_jpg, "S:/spiffs/img_lvgl_logo.jpg");
+    lv_obj_align_to(img_jpg, NULL, LV_ALIGN_RIGHT_MID, 0, 0);
+
+    // add qr
+    lv_color_t bg_color = lv_palette_lighten(LV_PALETTE_LIGHT_BLUE, 5);
+    lv_color_t fg_color = lv_palette_darken(LV_PALETTE_BLUE, 4);
+    lv_obj_t *qr = lv_qrcode_create(ui_Screen1, 80, fg_color, bg_color);
+    const char *data = "https://lvgl.io";
+    lv_qrcode_update(qr, data, strlen(data));
+    lv_obj_align_to(qr, NULL, LV_ALIGN_BOTTOM_MID, 0, 0);
 }
